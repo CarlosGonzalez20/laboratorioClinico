ff84c954400b50586b90144cad8f57d9392dbec7
Ammy C <Ammy C@AMMY>
2024-07-25 19:57:58 -0600

    Mi parte de los formulairos

---
+++ clinica/clinica/Clinica.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace clinica
+{
+    partial class Clinica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MTCMBCLINIC = new MaterialSkin.Controls.MaterialComboBox();
+            this.MTEst = new MaterialSkin.Controls.MaterialTextBox();
+            this.MTCOR = new MaterialSkin.Controls.MaterialTextBox();
+            this.MTTELE = new MaterialSkin.Controls.MaterialTextBox();
+            this.MTDir = new MaterialSkin.Controls.MaterialTextBox();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.MTCorreo = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            this.MTTEL = new MaterialSkin.Controls.MaterialLabel();
+            this.MTHA = new MaterialSkin.Controls.MaterialLabel();
+            this.MTDirec = new MaterialSkin.Controls.MaterialLabel();
+            this.MTNum_Especialidad = new MaterialSkin.Controls.MaterialLabel();
+            this.MTNom_clinic = new MaterialSkin.Controls.MaterialTextBox();
+            this.MLNom_Clinic = new MaterialSkin.Controls.MaterialLabel();
+            this.MTAREA = new MaterialSkin.Controls.MaterialTextBox();
+            this.MLNum_Clinic = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // MTCMBCLINIC
+            // 
+            this.MTCMBCLINIC.AutoResize = false;
+            this.MTCMBCLINIC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MTCMBCLINIC.Depth = 0;
+            this.MTCMBCLINIC.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.MTCMBCLINIC.DropDownHeight = 174;
+            this.MTCMBCLINIC.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.MTCMBCLINIC.DropDownWidth = 121;
+            this.MTCMBCLINIC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.MTCMBCLINIC.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.MTCMBCLINIC.FormattingEnabled = true;
+            this.MTCMBCLINIC.IntegralHeight = false;
+            this.MTCMBCLINIC.ItemHeight = 43;
+            this.MTCMBCLINIC.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.MTCMBCLINIC.Location = new System.Drawing.Point(298, 91);
+            this.MTCMBCLINIC.MaxDropDownItems = 4;
+            this.MTCMBCLINIC.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTCMBCLINIC.Name = "MTCMBCLINIC";
+            this.MTCMBCLINIC.Size = new System.Drawing.Size(200, 49);
+            this.MTCMBCLINIC.StartIndex = 0;
+            this.MTCMBCLINIC.TabIndex = 34;
+            // 
+            // MTEst
+            // 
+            this.MTEst.AnimateReadOnly = false;
+            this.MTEst.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MTEst.Depth = 0;
+            this.MTEst.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTEst.LeadingIcon = null;
+            this.MTEst.Location = new System.Drawing.Point(722, 270);
+            this.MTEst.MaxLength = 50;
+            this.MTEst.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTEst.Multiline = false;
+            this.MTEst.Name = "MTEst";
+            this.MTEst.Size = new System.Drawing.Size(200, 50);
+            this.MTEst.TabIndex = 33;
+            this.MTEst.Text = "";
+            this.MTEst.TrailingIcon = null;
+            // 
+            // MTCOR
+            // 
+            this.MTCOR.AnimateReadOnly = false;
+            this.MTCOR.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MTCOR.Depth = 0;
+            this.MTCOR.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTCOR.LeadingIcon = null;
+            this.MTCOR.Location = new System.Drawing.Point(722, 188);
+            this.MTCOR.MaxLength = 50;
+            this.MTCOR.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTCOR.Multiline = false;
+            this.MTCOR.Name = "MTCOR";
+            this.MTCOR.Size = new System.Drawing.Size(200, 50);
+            this.MTCOR.TabIndex = 32;
+            this.MTCOR.Text = "";
+            this.MTCOR.TrailingIcon = null;
+            // 
+            // MTTELE
+            // 
+            this.MTTELE.AnimateReadOnly = false;
+            this.MTTELE.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MTTELE.Depth = 0;
+            this.MTTELE.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTTELE.LeadingIcon = null;
+            this.MTTELE.Location = new System.Drawing.Point(722, 90);
+            this.MTTELE.MaxLength = 50;
+            this.MTTELE.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTTELE.Multiline = false;
+            this.MTTELE.Name = "MTTELE";
+            this.MTTELE.Size = new System.Drawing.Size(200, 50);
+            this.MTTELE.TabIndex = 31;
+            this.MTTELE.Text = "";
+            this.MTTELE.TrailingIcon = null;
+            // 
+            // MTDir
+            // 
+            this.MTDir.AnimateReadOnly = false;
+            this.MTDir.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MTDir.Depth = 0;
+            this.MTDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTDir.LeadingIcon = null;
+            this.MTDir.Location = new System.Drawing.Point(298, 338);
+            this.MTDir.MaxLength = 50;
+            this.MTDir.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTDir.Multiline = false;
+            this.MTDir.Name = "MTDir";
+            this.MTDir.Size = new System.Drawing.Size(200, 50);
+            this.MTDir.TabIndex = 30;
+            this.MTDir.Text = "";
+            this.MTDir.TrailingIcon = null;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(605, 299);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(58, 19);
+            this.materialLabel1.TabIndex = 29;
+            this.materialLabel1.Text = "Estado: ";
+            // 
+            // MTCorreo
+            // 
+            this.MTCorreo.AutoSize = true;
+            this.MTCorreo.Depth = 0;
+            this.MTCorreo.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTCorreo.Location = new System.Drawing.Point(557, 191);
+            this.MTCorreo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MTCorreo.Name = "MTCorreo";
+            this.MTCorreo.Size = new System.Drawing.Size(133, 19);
+            this.MTCorreo.TabIndex = 28;
+            this.MTCorreo.Text = "Correo Electrónico:";
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.AutoSize = true;
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel4.Location = new System.Drawing.Point(493, 132);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(5, 19);
+            this.materialLabel4.TabIndex = 27;
+            this.materialLabel4.Text = " ";
+            // 
+            // MTTEL
+            // 
+            this.MTTEL.AutoSize = true;
+            this.MTTEL.Depth = 0;
+            this.MTTEL.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTTEL.Location = new System.Drawing.Point(577, 102);
+            this.MTTEL.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MTTEL.Name = "MTTEL";
+            this.MTTEL.Size = new System.Drawing.Size(68, 19);
+            this.MTTEL.TabIndex = 26;
+            this.MTTEL.Text = "Teléfono:";
+            // 
+            // MTHA
+            // 
+            this.MTHA.AutoSize = true;
+            this.MTHA.Depth = 0;
+            this.MTHA.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTHA.Location = new System.Drawing.Point(33, 435);
+            this.MTHA.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MTHA.Name = "MTHA";
+            this.MTHA.Size = new System.Drawing.Size(147, 19);
+            this.MTHA.TabIndex = 25;
+            this.MTHA.Text = "Horario de actividad:";
+            // 
+            // MTDirec
+            // 
+            this.MTDirec.AutoSize = true;
+            this.MTDirec.Depth = 0;
+            this.MTDirec.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTDirec.Location = new System.Drawing.Point(33, 350);
+            this.MTDirec.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MTDirec.Name = "MTDirec";
+            this.MTDirec.Size = new System.Drawing.Size(71, 19);
+            this.MTDirec.TabIndex = 24;
+            this.MTDirec.Text = "Dirección:";
+            // 
+            // MTNum_Especialidad
+            // 
+            this.MTNum_Especialidad.AutoSize = true;
+            this.MTNum_Especialidad.Depth = 0;
+            this.MTNum_Especialidad.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTNum_Especialidad.Location = new System.Drawing.Point(32, 270);
+            this.MTNum_Especialidad.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MTNum_Especialidad.Name = "MTNum_Especialidad";
+            this.MTNum_Especialidad.Size = new System.Drawing.Size(233, 19);
+            this.MTNum_Especialidad.TabIndex = 23;
+            this.MTNum_Especialidad.Text = "Número de Area de Especialidad:";
+            // 
+            // MTNom_clinic
+            // 
+            this.MTNom_clinic.AnimateReadOnly = false;
+            this.MTNom_clinic.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MTNom_clinic.Depth = 0;
+            this.MTNom_clinic.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTNom_clinic.LeadingIcon = null;
+            this.MTNom_clinic.Location = new System.Drawing.Point(298, 178);
+            this.MTNom_clinic.MaxLength = 50;
+            this.MTNom_clinic.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTNom_clinic.Multiline = false;
+            this.MTNom_clinic.Name = "MTNom_clinic";
+            this.MTNom_clinic.Size = new System.Drawing.Size(200, 50);
+            this.MTNom_clinic.TabIndex = 22;
+            this.MTNom_clinic.Text = "";
+            this.MTNom_clinic.TrailingIcon = null;
+            // 
+            // MLNom_Clinic
+            // 
+            this.MLNom_Clinic.AutoSize = true;
+            this.MLNom_Clinic.Depth = 0;
+            this.MLNom_Clinic.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MLNom_Clinic.Location = new System.Drawing.Point(32, 191);
+            this.MLNom_Clinic.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MLNom_Clinic.Name = "MLNom_Clinic";
+            this.MLNom_Clinic.Size = new System.Drawing.Size(138, 19);
+            this.MLNom_Clinic.TabIndex = 21;
+            this.MLNom_Clinic.Text = "Nombre de Clinica: ";
+            // 
+            // MTAREA
+            // 
+            this.MTAREA.AnimateReadOnly = false;
+            this.MTAREA.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MTAREA.Depth = 0;
+            this.MTAREA.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MTAREA.LeadingIcon = null;
+            this.MTAREA.Location = new System.Drawing.Point(298, 268);
+            this.MTAREA.MaxLength = 50;
+            this.MTAREA.MouseState = MaterialSkin.MouseState.OUT;
+            this.MTAREA.Multiline = false;
+            this.MTAREA.Name = "MTAREA";
+            this.MTAREA.Size = new System.Drawing.Size(200, 50);
+            this.MTAREA.TabIndex = 20;
+            this.MTAREA.Text = "";
+            this.MTAREA.TrailingIcon = null;
+            // 
+            // MLNum_Clinic
+            // 
+            this.MLNum_Clinic.AutoSize = true;
+            this.MLNum_Clinic.Depth = 0;
+            this.MLNum_Clinic.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.MLNum_Clinic.Location = new System.Drawing.Point(32, 99);
+            this.MLNum_Clinic.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MLNum_Clinic.Name = "MLNum_Clinic";
+            this.MLNum_Clinic.Size = new System.Drawing.Size(138, 19);
+            this.MLNum_Clinic.TabIndex = 19;
+            this.MLNum_Clinic.Text = "Número de Clinica: ";
+            // 
+            // Clinica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 545);
+            this.Controls.Add(this.MTCMBCLINIC);
+            this.Controls.Add(this.MTEst);
+            this.Controls.Add(this.MTCOR);
+            this.Controls.Add(this.MTTELE);
+            this.Controls.Add(this.MTDir);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.MTCorreo);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.MTTEL);
+            this.Controls.Add(this.MTHA);
+            this.Controls.Add(this.MTDirec);
+            this.Controls.Add(this.MTNum_Especialidad);
+            this.Controls.Add(this.MTNom_clinic);
+            this.Controls.Add(this.MLNom_Clinic);
+            this.Controls.Add(this.MTAREA);
+            this.Controls.Add(this.MLNum_Clinic);
+            this.Name = "Clinica";
+            this.Text = "Clinica";
+            this.Load += new System.EventHandler(this.Clinica_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialComboBox MTCMBCLINIC;
+        private MaterialSkin.Controls.MaterialTextBox MTEst;
+        private MaterialSkin.Controls.MaterialTextBox MTCOR;
+        private MaterialSkin.Controls.MaterialTextBox MTTELE;
+        private MaterialSkin.Controls.MaterialTextBox MTDir;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel MTCorreo;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialLabel MTTEL;
+        private MaterialSkin.Controls.MaterialLabel MTHA;
+        private MaterialSkin.Controls.MaterialLabel MTDirec;
+        private MaterialSkin.Controls.MaterialLabel MTNum_Especialidad;
+        private MaterialSkin.Controls.MaterialTextBox MTNom_clinic;
+        private MaterialSkin.Controls.MaterialLabel MLNom_Clinic;
+        private MaterialSkin.Controls.MaterialTextBox MTAREA;
+        private MaterialSkin.Controls.MaterialLabel MLNum_Clinic;
+    }
+}
