d85d2bb7cb13400441c36b3370611866cc50b4fb
EMERZON <egarciat16@miumg.edu.gt>
2024-07-30 18:33:53 -0600

    tabla

---
+++ clinica/clinica/Empleado.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace clinica
+{
+    partial class Empleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Empleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 538);
+            this.Name = "Empleado";
+            this.Text = "Empleado";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
